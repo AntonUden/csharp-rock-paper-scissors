037d818d26fbf62d9ebe9e607fb824315661f19d
BuildTools <udenanton@hotmail.com>
2019-05-10 14:40:00 +0200

    Removed unused using

--- rock paper scissors/Winner.cs
+++ rock paper scissors/Winner.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace rock_paper_scissors {
+﻿namespace rock_paper_scissors {
 	enum Winner {
 		Player, Computer, Tie
 	}
